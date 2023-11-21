56b27b777809b1538803cdb7cd8232c655f1b20e
Andres Jimenez <andresjimenez884@gmail.com>
2023-11-20 20:50:14 -0400

    Update

--- src/test/react.test.jsx
+++ src/test/react.test.jsx
@@ -4,51 +4,52 @@ import { Calculadora, operaciones, igualdad } from "../calculator";
 
 
 describe("calculadora", () => {
+
   afterEach(cleanup);
   it("debe renderizar", () => {
     render(<Calculadora />);
-  });
+  })
 
   it("debe renderizar el titulo", () => {
     render(<Calculadora />);
     screen.getByText("Calculadora");
-  });
+  })
 
   it("debe renderizar un numero", () => {
     render(<Calculadora />);
     const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     numeros.forEach((numero) => {
       screen.getByText(numero);
-    });
-  });
+    })
+  })
 
   it("debe tener 4 filas", () => {
     render(<Calculadora />);
 
     const rows = screen.getAllByRole("row");
-    expect(rows.length).toBe(4);
+    expect(rows.length).toBe(4)
   });
 
   it("debe tener las operaciones", () => {
-    render(<Calculadora />);
+    render(<Calculadora />)
 
     operaciones.forEach((operaciones) => {
       screen.getByText(operaciones);
-    });
-  });
+    })
+  })
 
   it("debe tener simbolo de igualdad =", () => {
-    render(<Calculadora />);
-    screen.getByText("=");
+    render(<Calculadora />)
+    screen.getByText("=")
   })
 
   it("debe renderizar un input", () => {
-    render(<Calculadora />);
+    render(<Calculadora />)
     screen.getByRole("textbox")
   });
 
   it("debe renderizar al presionar un numero", () => {
-    render(<Calculadora />);
+    render(<Calculadora />)
     
     const uno = screen.getByText("1")
     fireEvent.click(uno)
@@ -74,10 +75,10 @@ describe("calculadora", () => {
 
     const input = screen.getByRole("textbox")
     expect(input.value).toBe("1234")
-  });
+  })
 
   it("debe mostrar los numeros y la operaciones", () => {
-    render(<Calculadora />);
+    render(<Calculadora />)
     
     const uno = screen.getByText("1")
     fireEvent.click(uno)
@@ -92,7 +93,7 @@ describe("calculadora", () => {
   })
 
   it("debe mostrar el resultado de la operacion" , () => {
-    render(<Calculadora />);
+    render(<Calculadora />)
     
     const uno = screen.getByText("1")
     fireEvent.click(uno)
